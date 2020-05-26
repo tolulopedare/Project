445dab88a82463e9992cb988cb71eb9858e43dde
tolulopedare <62181947+tolulopedare@users.noreply.github.com>
2020-05-26 19:54:22 +0100

    Initial commit

---
+++ JS/JS.js
@@ -0,0 +1,23 @@
+/**
+* GLOBAL JAVASCRIPT
+*/
+
+function search() {
+    prompt("What are you looking for?")
+}
+
+
+function callToAction() {
+    alert("Thank you!")
+}
+
+
+function submitForm() {
+    alert("You are signed in!")
+}
+
+function requestPassword() {
+    prompt("Tell us your email")
+}
+
+document.querySelector("#footer > div > div > div.Footer1__paymentOptions")
